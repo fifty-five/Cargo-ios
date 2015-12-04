8f4c897359bdfa779cceda96b7774dd1c4fa80e6
louis <louis.chavane@gmail.com>
2015-12-04 09:45:35 +0100

    add Constants

--- Cargo/Models/CARConstants.m
+++ Cargo/Models/CARConstants.m
@@ -10,4 +10,34 @@
 
 @implementation CARConstants
 
+#pragma tracker
+NSString *const ENABLE_DEBUG = @"enableDebug";
+NSString *const ENABLE_OPTOUT = @"enableOptOut";
+NSString *const DISABLE_TRACKING = @"disableTracking";
+NSString *const DISPATCH_PERIOD = @"dispatchPeriod";
+
+#pragma screen
+NSString *const SCREEN_NAME = @"screenName";
+
+#pragma event
+NSString *const EVENT_NAME = @"eventName";
+
+#pragma user
+NSString *const USER_GOOGLE_ID = @"userGoogleId";
+NSString *const USER_FACEBOOK_ID = @"userFacebookId";
+NSString *const USER_ID = @"userId";
+
+#pragma transaction
+
+NSString *const TRANSACTION_ID = @"transactionId";
+NSString *const TRANSACTION_TOTAL = @"transactionTotal";
+NSString *const TRANSACTION_PRODUCTS = @"transactionProducts";
+NSString *const TRANSACTION_PRODUCT_NAME = @"name";
+NSString *const TRANSACTION_PRODUCT_SKU = @"sku";
+NSString *const TRANSACTION_PRODUCT_PRICE = @"price";
+NSString *const TRANSACTION_PRODUCT_CATEGORY = @"category";
+NSString *const TRANSACTION_PRODUCT_QUANTITY = @"quantity";
+
+
+
 @end

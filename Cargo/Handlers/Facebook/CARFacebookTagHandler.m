b2f1ffb35b32ef078c11b12f0cf99af3d0c92c6c
Julien Gil <jgil@student.42.fr>
2016-05-13 16:34:21 +0200

    facebook handler commented

--- Cargo/Handlers/Facebook/CARFacebookTagHandler.m
+++ Cargo/Handlers/Facebook/CARFacebookTagHandler.m
@@ -13,7 +13,10 @@
 @implementation CARFacebookTagHandler
 
 
-
+// The runtime sends the load message very soon after the class object
+// is loaded in the process's address space. (http://stackoverflow.com/a/13326633)
+//
+// Instanciate the handler, and register its callback methods to GTM through a Cargo method
 +(void)load{
     CARFacebookTagHandler *handler = [[CARFacebookTagHandler alloc] init];
     [Cargo registerTagHandler:handler withKey:@"FB_init"];
@@ -21,7 +24,10 @@
 }
 
 
-
+// This one will be called after a tag has been sent
+//
+// @param tagName       The method you aime to call (this should be define in GTM interface)
+// @param parameters    A dictionary key-object used as a way to give parameters to the class method aimed here
 -(void) execute:(NSString *)tagName parameters:(NSDictionary *)parameters{
     [super execute:tagName parameters:parameters];
     if([tagName isEqualToString:@"FB_init"]){
@@ -34,6 +40,7 @@
 }
 
 
+// Called in +load method, setup what is needed for Cargo and the facebook SDK
 - (id)init
 {
     if (self = [super init]) {
@@ -47,26 +54,28 @@
 }
 
 
-
-
 - (void)validate
 {
     // Nothing is required
     self.valid = TRUE;
 }
 
+
+
+// Initialize Facebook with required parameters
 -(void) init:(NSDictionary*) parameters{
-    
-    //Initialize Tune with required parameters
+
+    // get the application id Facebook gave you when you created your facebook app from parameters
     NSString *applicationId = [parameters objectForKey:@"applicationId"];
     
-    
+    // setup the app id to the fb SDK
     if (applicationId){
         [self.fbAppEvents setLoggingOverrideAppID:applicationId];
         FIFLog(kTAGLoggerLogLevelInfo, @" Facebook appId set to %@ ", applicationId);
 
     }
     
+    // let the fb sdk know that your app has been launched
     [self.fbAppEvents activateApp];
     self.initialized = TRUE;
     
@@ -78,7 +87,9 @@
     
 }
 
+// Send an event to facebook SDK
 -(void) tagEvent:(NSDictionary*) parameters{
+    // retrieves the custom event from parameters and send its to FB
     [FBSDKAppEvents logEvent:[parameters objectForKey:EVENT_NAME]];
 }
 

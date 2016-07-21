f74fe516e57547289f73865d030cb934109721d7
Julien Gil <jgil@student.42.fr>
2016-07-21 15:54:44 +0200

    created the tests but still empty

--- CargoTest/Firebase/FirebaseTests.m
+++ CargoTest/Firebase/FirebaseTests.m
@@ -1,5 +1,5 @@
 //
-//  TuneTests.m
+//  FirebaseTests.m
 //  Cargo
 //
 //  Created by louis chavane on 08/10/15.
@@ -8,8 +8,8 @@
 
 #import <XCTest/XCTest.h>
 #import "TAGManager.h"
-#import "CARTuneTagHandler.h"
-#import <Tune/Tune.h>
+#import "CARFirebaseTagHandler.h"
+#import <Firebase.h>
 #import "CARConstants.h"
 
 #define HC_SHORTHAND
@@ -21,24 +21,22 @@
 
 
 
-@interface TuneTests : XCTestCase
+@interface FirebaseTests : XCTestCase
 
-@property CARTuneTagHandler *handler;
-@property Class tuneClassMock;
+@property CARFirebaseTagHandler* handler;
 
 @end
 
 
 
-@implementation TuneTests
-
+@implementation FirebaseTests
 
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    _handler = [[CARTuneTagHandler alloc] init];
-    _tuneClassMock = mockClass([Tune class]);
-    [_handler setTuneClass:_tuneClassMock];
+    _handler = [[CARFirebaseTagHandler alloc] init];
+
+    
 }
 
 
@@ -49,72 +47,5 @@
     [super tearDown];
 }
 
-
--(void) testInitTune {
-    NSDictionary * dict = [[NSDictionary alloc ] initWithObjectsAndKeys:@"234",@"advertiserId",@"456",@"conversionKey", nil];
-    [_handler execute:@"Tune_init" parameters:dict];
-    [verifyCount(_tuneClassMock, times(1)) initializeWithTuneAdvertiserId:@"234" tuneConversionKey:@"456"];
-    
-    XCTAssertTrue(_handler.initialized);
-    
-}
-
--(void) testInitTuneWithoutRequiredParams {
-    NSDictionary * dict = [[NSDictionary alloc ] init];
-    [_handler execute:@"Tune_init" parameters:dict];
-    [verifyCount(_tuneClassMock, times(0)) initializeWithTuneAdvertiserId:anything() tuneConversionKey:anything()];
-    XCTAssertFalse(_handler.initialized);
-    
-}
-
--(void) testTuneIdentify {
-    NSDictionary * dict = [[NSDictionary alloc ] initWithObjectsAndKeys:@"234", USER_ID, @"55", USER_AGE, @"Male", USER_GENDER, @"01223456789", USER_FACEBOOK_ID, nil];
-    [_handler execute:@"Tune_identify" parameters:dict];
-    
-    [verifyCount(_tuneClassMock, times(1)) setUserId:@"234"];
-    [verifyCount(_tuneClassMock, times(1)) setFacebookUserId:@"01223456789"];
-    [verifyCount(_tuneClassMock, times(1)) setGender:TuneGenderMale];
-    [verifyCount(_tuneClassMock, times(1)) setAge:55];
-    [verifyCount(_tuneClassMock, times(0)) setTwitterUserId:anything()];
-    [verifyCount(_tuneClassMock, times(0)) setGoogleUserId:anything()];
-}
-
--(void) testTuneIdentifyWithoutUserId {
-    NSDictionary * dict = [[NSDictionary alloc ] initWithObjectsAndKeys: @"55", USER_AGE, @"Male", USER_GENDER, @"01223456789", USER_FACEBOOK_ID, nil];
-    [_handler execute:@"Tune_identify" parameters:dict];
-    
-    [verifyCount(_tuneClassMock, times(0)) setUserId:anything()];
-    [verifyCount(_tuneClassMock, times(0)) setFacebookUserId:anything()];
-    [verifyCount(_tuneClassMock, times(0)) setGender:anything()];
-    [verifyCount(_tuneClassMock, times(0)) setAge:anything()];
-    [verifyCount(_tuneClassMock, times(0)) setTwitterUserId:anything()];
-    [verifyCount(_tuneClassMock, times(0)) setGoogleUserId:anything()];
-}
-
--(void) testTuneIdentifyWithWeirdGender {
-    NSDictionary * dict = [[NSDictionary alloc ] initWithObjectsAndKeys: @"55", USER_ID, @"skdjhf", USER_GENDER, nil];
-    [_handler execute:@"Tune_identify" parameters:dict];
-    
-    [verifyCount(_tuneClassMock, times(1)) setUserId:@"55"];
-    [verifyCount(_tuneClassMock, times(1)) setGender:TuneGenderUnknown];
-}
-
--(void) testScreenName {
-    NSDictionary * dict = [[NSDictionary alloc ]
-                           initWithObjectsAndKeys:@"A screen",@"screenName", nil];
-
-    
-    [_handler execute:@"Tune_tagScreen" parameters:dict];
-    [verifyCount(_tuneClassMock, times(1)) measureEvent:anything()];
-}
-
--(void) testTageEvent {
-    NSDictionary * dict = [[NSDictionary alloc ]
-                           initWithObjectsAndKeys:@"aRandomEventName",EVENT_NAME, nil];
-    
-    
-    [_handler execute:@"Tune_tagEvent" parameters:dict];
-    [verifyCount(_tuneClassMock, times(1)) measureEvent:anything()];
-}
 
 @end

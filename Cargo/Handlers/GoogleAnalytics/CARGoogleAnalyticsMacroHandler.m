b4d5661be09cb287bbd83605c988a81838f9c46e
juliengil <julien.gil@fifty-five.com>
2016-11-18 16:20:57 +0100

    CARGoogleAnalyticsHandler commented

--- Cargo/Handlers/GoogleAnalytics/CARGoogleAnalyticsMacroHandler.m
+++ Cargo/Handlers/GoogleAnalytics/CARGoogleAnalyticsMacroHandler.m
@@ -12,16 +12,28 @@
 #import "FIFLogger.h"
 #import "CARConstants.h"
 
+/**
+ The class which handles interactions with the Google Analytics SDK.
+ */
 @implementation CARGoogleAnalyticsMacroHandler
 
-
+/**
+ Called on runtime to instantiate the handler.
+ Register the macros to the container. After a [dataLayer push:@{}],
+ these will trigger the execute method of this handler.
+ */
 +(void)load{
     CARGoogleAnalyticsMacroHandler * handler = [[CARGoogleAnalyticsMacroHandler alloc] init];
     [Cargo registerMacroHandler:handler forMacro:@"userGoogleId"];
     [Cargo registerMacroHandler:handler forMacro:@"idfa"];
 }
 
-
+/**
+ Instantiate the handler with its key and name properties
+ Initialize its attribute to the default values.
+ 
+ @return the instance of the Google Anayltics Macro handler
+ */
 - (id)init
 {
     if (self = [super init]) {
@@ -35,7 +47,20 @@
 
 
 
-
+/**
+ * Returns an object which is the calculated value of the macro.
+ * Handler is given the macro name and a dictionary of named parameters.
+ *
+ * @param macroName The same name by which the handler was registered. It
+ *     is provided as a convenience to allow a single handler to be registered
+ *     for multiple function call macros.
+ * @param parameters The named parameters for the function call. The
+ *     dictionary may contain <code>NSString</code>, <code>NSNumber</code>
+ *     (double, int, or boolean), <code>NSDictionary</code>, or
+ *     <code>NSArray</code>.
+ * @return The evaluated result, which can be an <code>NSString</code> or
+ *     <code>NSNumber</code>.
+ */
 #pragma mark - GTMFunctionMacroCallBack
 - (id)valueForMacro:(NSString *)functionName parameters:(NSDictionary *)parameters {
     if ([functionName isEqualToString:USER_GOOGLE_ID]) {
--- Cargo/Handlers/GoogleAnalytics/CARGoogleAnalyticsTagHandler.m
+++ Cargo/Handlers/GoogleAnalytics/CARGoogleAnalyticsTagHandler.m
@@ -20,95 +20,167 @@
 
 @end
 
-
+/**
+ *  The class handle all interaction and event calls shipped to Google Analytics.
+ */
 @implementation CARGoogleAnalyticsTagHandler
 
+/* *********************************** Variables Declaration ************************************ */
 
+/** Constants used to define callbacks in the register and in the execute method */
+NSString* GA_init = @"GA_init";
+NSString* GA_set = @"GA_set";
+NSString* GA_upload = @"GA_upload";
+
+NSString* TRACKING_ID = @"trackingId";
+
+
+/* ********************************** Handler core methods ************************************** */
+
+/**
+ Called on runtime to instantiate the handler.
+ Register the callbacks to the container. After a [dataLayer push:@{}],
+ these will trigger the execute method of this handler.
+ */
 +(void)load{
     CARGoogleAnalyticsTagHandler *handler = [[CARGoogleAnalyticsTagHandler alloc] init];
-    
-    [Cargo registerTagHandler:handler withKey:@"GA_init"];
-    [Cargo registerTagHandler:handler withKey:@"GA_set"];
-    [Cargo registerTagHandler:handler withKey:@"GA_upload"];
+
+    [Cargo registerTagHandler:handler withKey:GA_init];
+    [Cargo registerTagHandler:handler withKey:GA_set];
+    [Cargo registerTagHandler:handler withKey:GA_upload];
 }
 
-
-
--(void) execute:(NSString *)tagName parameters:(NSDictionary *)parameters{
-    [super execute:tagName parameters:parameters];
-    if([tagName isEqualToString:@"GA_set"]){
-        [self set:parameters];
-    }
-    else if ([tagName isEqualToString:@"GA_init"]){
-        [self init:parameters];
-    }
-    else if ([tagName isEqualToString:@"GA_upload"]){
-        [self upload:parameters];
-    }
-}
-
-
-- (id)init
-{
+/**
+ Instantiate the handler with its key and name properties
+ Initialize its attribute to the default values.
+ 
+ @return the instance of the Googla Analytics handler
+ */
+- (id)init{
     if (self = [super init]) {
         self.key = @"GA";
         self.name = @"Google Analytics";
         self.valid = NO;
         self.initialized = NO;
+
         self.tracker = [[GAI sharedInstance] defaultTracker];
         self.instance = [GAI sharedInstance];
-        
     }
     return self;
 }
 
+/**
+ Call back from GTM container to call a specific method
+ after a function tag and associated parameters are received
+ 
+ @param tagName The tag name of the aimed method
+ @param parameters Dictionary of parameters
+ */
+-(void) execute:(NSString*)tagName parameters:(NSDictionary *)parameters{
+    [super execute:tagName parameters:parameters];
 
+    if ([tagName isEqualToString:GA_init]){
+        [self init:parameters];
+    }
+    else if (self.initialized) {
+        if([tagName isEqualToString:GA_set]){
+            [self set:parameters];
+        }
+        else if ([tagName isEqualToString:GA_upload]){
+            [self upload:parameters];
+        }
+    }
+    else
+        [self.logger logUninitializedFramework];
+}
 
-
+/**
+ Called in registerHandlers to validate a handler and check for its initialization.
+ */
 - (void)validate
 {
     // Nothing is required
     self.valid = TRUE;
 }
 
+
+/* ************************************ SDK initialization ************************************** */
+
+/**
+ The method you need to call first. Allow you to initialize Google Analytics SDK
+ Register the tracking ID to the Google Analytics SDK.
+ 
+ @param parameters :
+  - trackingId: UAID Google Analytics gives when you register your app
+ */
 -(void)init:(NSDictionary*)parameters{
-    NSString* trackingId = [parameters objectForKey:@"trackingId"];
+    NSString* trackingId = [CARUtils castToNSString:[parameters objectForKey:TRACKING_ID]];
+
     if(trackingId){
         [self.instance trackerWithTrackingId:trackingId];
+        self.initialized = TRUE;
     }
-    [self set:parameters];
+    else
+        [self.logger logMissingParam:TRACKING_ID inMethod:GA_init];
 }
 
+
+/* ****************************************** Tracking ****************************************** */
+
+/**
+ Called to set optional parameters. If a parameter is not defined, its value will be set to default
+
+ @param parameters :
+  - trackUncaughtExceptions: boolean set to true by default
+  - allowIdfaCollection: boolean set to true by default
+  - dispatchInterval: Double set to 30 by default. Time interval before sending pending hits
+ */
 - (void)set:(NSDictionary *)parameters {
+    /** default values for the optional parameters */
+    BOOL    trackException = TRUE;
+    BOOL    idfaCollection = TRUE;
+    double  dispInterval = 30;
 
-    NSString * trackUncaughtException = [parameters objectForKey:@"trackUncaughtExceptions"];
-    if([trackUncaughtException boolValue]){
-        [self.instance trackUncaughtExceptions];
-    }
-    
-    NSString * allowIdfaCollection = [parameters objectForKey:@"allowIdfaCollection"];
-    if([allowIdfaCollection boolValue]){
-        [self.tracker allowIDFACollection];
-    }
-    
-    NSString * dispatchInterval = [parameters objectForKey:@"dispatchInterval"];
-    if(dispatchInterval){
-        NSNumber *interval = [CARUtils castToNSNumber:dispatchInterval];
-        [self.instance setDispatchInterval:[interval integerValue]];
-    }
-    
-    
+    // retrieve the trackUncaughtExceptions value if existant, or set to the default value
+    NSString* trackUncaughtException = [CARUtils castToNSString:
+                                         [parameters objectForKey:@"trackUncaughtExceptions"]];
+    if(trackUncaughtException)
+        trackException = [trackUncaughtException boolValue];
+    self.instance.trackUncaughtExceptions = trackException;
+    [self.logger logParamSetWithSuccess:@"trackUncaughtExceptions"
+                              withValue:[NSNumber numberWithBool:trackException]];
 
-    
+    // retrieve the allowIdfaCollection value if existant, or set to the default value
+    NSString* allowIdfaCollection = [CARUtils castToNSString:
+                                         [parameters objectForKey:@"allowIdfaCollection"]];
+    if(allowIdfaCollection)
+        idfaCollection = [allowIdfaCollection boolValue];
+    self.tracker.allowIDFACollection = idfaCollection;
+    [self.logger logParamSetWithSuccess:@"allowIdfaCollection"
+                              withValue: [NSNumber numberWithBool:idfaCollection]];
+
+    // retrieve the dispatchInterval value if existant, or set to the default value
+    NSNumber* dispatchInterval = [CARUtils castToNSNumber:
+                                      [parameters objectForKey:@"dispatchInterval"]];
+    if(dispatchInterval)
+        dispInterval = [dispatchInterval integerValue];
+    self.instance.dispatchInterval = dispInterval;
+    [self.logger logParamSetWithSuccess:@"dispatchInterval"
+                              withValue:[NSNumber numberWithInteger:dispInterval]];
 }
 
+
+/**
+ Call it in order to force a dispatch
+
+ @param parameters none are required
+ */
 - (void)upload:(NSDictionary *)parameters {
     (void)parameters;
 
     //Upload
     [self.instance dispatch];
-    FIFLog(kTAGLoggerLogLevelInfo, @"%@ upload success.",
-           self.name);
+    FIFLog(kTAGLoggerLogLevelInfo, @"%@ upload success.", self.name);
     
 }
 

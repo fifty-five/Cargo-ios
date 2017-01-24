b202b7a1452fb81d8b76f07648d970f5234efa97
Julien Gil <julien.gil@fifty-five.com>
2017-01-24 11:25:34 +0100

    created a sample app for testing purposes

--- App/ViewController.m
+++ App/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "Cargo.h"
+#import "TAGManager.h"
+#import "TAGDataLayer.h"
 
 @interface ViewController ()
 
@@ -14,15 +17,35 @@
 
 @implementation ViewController
 
+TAGDataLayer *dataLayer;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    Cargo *cargo = [Cargo sharedHelper];
+    dataLayer = cargo.tagManager.dataLayer;
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)tagEventPressed{
+    [dataLayer push:@{@"event": @"tagEvent"}];
+}
+
+- (IBAction)tagScreenPressed{
+    [dataLayer push:@{@"event": @"tagScreen"}];
+}
+
+- (IBAction)tagPurchasePressed{
+    [dataLayer push:@{@"event": @"tagPurchase"}];
+}
+
+- (IBAction)tagUserPressed{
+    [dataLayer push:@{@"event": @"tagUser"}];
 }
 
 @end

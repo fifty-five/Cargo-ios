0337695c7c1d1b791e6bdd5fe20b2da0704a6043
Julien Gil <julien.gil@fifty-five.com>
2017-01-25 10:14:31 +0100

    creation of a new class to handle item objects sending through GTM

---
+++ Cargo/CARItem.h
@@ -0,0 +1,13 @@
+//
+//  CARItem.h
+//  Cargo
+//
+//  Created by Julien Gil on 24/01/2017.
+//  Copyright © 2017 55 SAS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CARItem : NSObject
+
+@end
---
+++ Cargo/CARItem.m
@@ -0,0 +1,13 @@
+//
+//  CARItem.m
+//  Cargo
+//
+//  Created by Julien Gil on 24/01/2017.
+//  Copyright © 2017 55 SAS. All rights reserved.
+//
+
+#import "CARItem.h"
+
+@implementation CARItem
+
+@end

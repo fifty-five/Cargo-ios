583d2fb3cccf021f075bd203ad7b1bd28e1924bf
Julien Gil <julien.gil@fifty-five.com>
2017-01-26 15:33:59 +0100

    deletion of old container and useless CARMacroHandler class + creation of Tags class and new container + google-services.plist

---
+++ Cargo/Tags.h
@@ -0,0 +1,13 @@
+//
+//  Tags.h
+//  Cargo
+//
+//  Created by Julien Gil on 25/01/2017.
+//  Copyright © 2017 55 SAS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tags : NSObject
+
+@end
---
+++ Cargo/Tags.m
@@ -0,0 +1,13 @@
+//
+//  Tags.m
+//  Cargo
+//
+//  Created by Julien Gil on 25/01/2017.
+//  Copyright © 2017 55 SAS. All rights reserved.
+//
+
+#import "Tags.h"
+
+@implementation Tags
+
+@end

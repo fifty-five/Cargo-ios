de4ad56a6120d65d970bc48ee1d5089a1d901fd5
Julien Gil <julien.gil@fifty-five.com>
2017-01-27 11:50:55 +0100

    documentation

--- Cargo/Handlers/CARTagHandler.m
+++ Cargo/Handlers/CARTagHandler.m
@@ -40,12 +40,19 @@
     self.valid = true;
 }
 
+/**
+ Sets the handler's name and key, initialize the logger instance, 
+ sets the loglevel of this one at the same level as cargo's, stores the handler into a dict in Cargo
+ 
+ @param handlerKey a short string describing the handler, and which will be used to call it
+ @param handlerName the name of the tool the handlers stands for.
+ */
 - (id)initWithKey:(NSString *)handlerKey andName:(NSString *)handlerName{
     self.key = handlerKey;
     self.name = handlerName;
     self.logger = [[FIFLogger alloc] initLogger:[self.key stringByAppendingString:@"_handler"]];
     [self.logger  setLevel:[[[Cargo sharedHelper] logger] level]];
-    [[Cargo sharedHelper] registerHandler:self forKey:self.key];
+    [[Cargo sharedHelper] registerHandler:self];
     self.valid = NO;
     self.initialized = NO;
 
